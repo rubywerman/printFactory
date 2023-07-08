714d1da6981198d79c0ca934e175d6bd0997f7dc
Ruby Werman <rubywerman@Rubys-MacBook-Pro.local>
2023-07-07 23:22:47 -0700

    create file size script

---
+++ print.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+
+// Desired file sizes in inches
+const fileSizes = [
+  { width: 5, height: 7 },
+  { width: 8, height: 10 },
+  { width: 9, height: 12 },
+  { width: 11, height: 14 },
+  { width: 16, height: 20 },
+  { width: 18, height: 24 },
+  { width: 24, height: 36 }
+];
+
+async function resizeImage(inputPath, outputPath, width, height) {
+  await sharp(inputPath)
+    .resize(width * 900, height * 900)
+    .withMetadata({ density: 300 })
+    .toFile(outputPath);
+}
+
+async function generateFileSizes(inputPath) {
+  try {
+
+    const outputFolder = 'output';
+    if (!fs.existsSync(outputFolder)) {
+      fs.mkdirSync(outputFolder);
+    }
+
+    for (const size of fileSizes) {
+      const { width, height } = size;
+
+      const outputPath = path.join(outputFolder, `${contentName}_${width}x${height}.jpg`);
+
+      await resizeImage(inputPath, outputPath, width, height);
+
+      console.log(`Generated ${outputPath}`);
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
+const contentName = 'westernwoman';
+const inputPath = `img/${contentName}-transformed.jpeg`;
+
+generateFileSizes(inputPath);
